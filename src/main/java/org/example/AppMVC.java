db9d792e842a65efeedac6275fe1adaafc74deb7
UBCh <u2b127@gmail.com>
2023-08-10 20:18:49 +0300

    itogo

--- src/main/java/org/example/AppMVC.java
+++ src/main/java/org/example/AppMVC.java
@@ -1,27 +1,12 @@
 package org.example;
 
-import org.example.domain.Task;
-import org.example.service.TaskService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-
-import java.util.List;
 
 @SpringBootApplication(scanBasePackages = "org.example")
 public class AppMVC {
     public static void main(String[] args) {
 	ConfigurableApplicationContext run = SpringApplication.run(AppMVC.class, args);
-
-	TaskService service = run.getBean(TaskService.class);
-	List<Task> all = service.findByAllTask(1, 10);
-	for (Task task : all) {
-	    System.out.println(task);
-	}
-
-	Task task = service.findByIdTask(9L);
-
-	System.out.println(task);
-
     }
 }
